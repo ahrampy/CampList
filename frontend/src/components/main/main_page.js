67df61aeb22b366e3537bc1dd3b6ecdf93d7d82b
your name here <ahrampy@gmail.com>
2019-11-18 11:52:21 -0800

    Init src directories

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>blmFriends</p>
+        <footer>
+          This is a footer
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
