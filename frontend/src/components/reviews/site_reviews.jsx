2be92db33f0f3fa2bf5cd80ee18075ea93285022
Will Smith <smithw1994@gmail.com>
2019-11-20 17:01:28 -0800

    Small changes to review form

--- frontend/src/components/reviews/site_reviews.jsx
+++ frontend/src/components/reviews/site_reviews.jsx
@@ -24,35 +24,53 @@ class SiteReviews extends React.Component {
   handleSubmit(e) {
     e.preventDefault()
     this.props.createReview(this.state)
+
+    this.setState({
+      body: '',
+      rating: '5'
+    })
   }
   
 
   render() {
     
     return(
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Rating (1-5)
-            <input 
-              type="number"
-              min="0"
-              max="5"
-              value={this.state.rating}
-              onChange={this.update("rating")}
-              id="rating"
-            />
-          </label>
-            <br/>
-          <label>Review </label>
-            <br/>
-          <textarea
-            required
-            cols="31"
-            value={this.state.body}
-            onChange={this.update("body")}
-          />
-          <br/>
-          <button type="submit" >Submit Test Review</button>
+      <div className="review-holder">
+        <p className="review-label">Submit your review here!</p>
+        <form onSubmit={this.handleSubmit} className="review-form">
+          <div className="rating-container">
+            <label className="rating-label">Rating (1-5)
+              <input 
+                className="rating-input"
+                type="number"
+                min="0"
+                max="5"
+                value={this.state.rating}
+                onChange={this.update("rating")}
+                id="rating"
+              />
+              {/* <span>
+                <input type="radio" name="rating" value="1" >1</input>
+                <input type="radio" name="rating" value="2" >2</input>
+                <input type="radio" name="rating" value="3" >3</input>
+                <input type="radio" name="rating" value="4" >4</input>
+                <input type="radio" name="rating" value="5" >5</input>
+              </span> */}
+            </label>
+          </div>
+          <div className="body-container">      
+            <textarea
+              className="body-input"
+              placeholder="Tell us about your stay"
+              required
+              cols="31"
+              value={this.state.body}
+              onChange={this.update("body")}
+            />           
+          </div>
+          <div className="submit-btn">
+            <button type="submit" className="btn">Submit Review</button>
+          </div>
         </form>
       </div>
     );
