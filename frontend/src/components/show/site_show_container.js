9a5aeec65981c4475003bae2b19639af20647d6d
Will Smith <smithw1994@gmail.com>
2019-11-21 20:43:17 -0800

    Fix bug where fetchUsers appears twice

--- frontend/src/components/show/site_show_container.js
+++ frontend/src/components/show/site_show_container.js
@@ -3,9 +3,6 @@ import { fetchSite } from '../../actions/site_actions';
 import { fetchUsers } from '../../actions/user_actions'
 import SiteShow from './site_show';
 
-import { fetchSiteReviews } from '../../actions/review_actions';
-import { fetchUsers } from '../../actions/user_actions';
-
 const mapStateToProps = (state, ownProps) => {
   // const siteId = parseInt(ownProps.match.params.siteId);
   // const site = seleteSite(state.entites, siteId);
