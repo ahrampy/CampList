3696f77a1bbeb46b2014ba8a8ee901375d257182
max <maxmus2223@gmail.com>
2019-12-03 09:30:45 -0800

    fix modal creation form bug

--- frontend/src/components/show/site_form_container.js
+++ frontend/src/components/show/site_form_container.js
@@ -3,10 +3,18 @@ import { createNewSite } from '../../actions/site_actions';
 import SiteForm from './site_form';
 import { openModal } from '../../actions/modal';
 
-const mapStateToProps = (state, { location }) => ({
-  lat: new URLSearchParams(location.search).get('lat'),
-  lng: new URLSearchParams(location.search).get('lng')
-});
+const mapStateToProps = (state, { location }) => {
+  let authorId
+  if (state.session.isAuthenticated) {
+    authorId = state.session.user.id
+  }
+  
+  return ({
+    lat: new URLSearchParams(location.search).get('lat'),
+    lng: new URLSearchParams(location.search).get('lng'),
+    authorId
+  })
+};
 
 const mapDispatchToProps = dispatch => ({
   createNewSite: site => dispatch(createNewSite(site)),
