33530d717505151a83dce124d71ec0285968b494
your name here <ahrampy@gmail.com>
2020-02-03 18:21:51 -0800

    clean up comments

--- frontend/src/components/maps/index_map.js
+++ frontend/src/components/maps/index_map.js
@@ -8,9 +8,9 @@ class IndexMap extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      showingInfoWindow: false, //Hides or the shows the infoWindow
-      activeMarker: {}, //Shows the active marker upon click
-      selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
     };
     this.onMarkerClick = this.onMarkerClick.bind(this)
     this.onClose = this.onClose.bind(this)
